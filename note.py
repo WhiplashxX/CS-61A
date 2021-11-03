247457ffd88e96e36b652318694e132a3f5501b5
WhiplashxX <54304074+WhiplashxX@users.noreply.github.com>
2021-11-03 10:56:58 +0800

    Create note.py

---
+++ note.py
@@ -0,0 +1,7 @@
+#An Environment is a sequence of frames
+#A name evaluates to the value bound to that name in the earliest  frame of the current environment in which that name is found
+from operator import mul
+def square(square):
+  return mul(square,square)
+square(-2)
+#see in https://pythontutor.com/visualize.html#mode=edit
